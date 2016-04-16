326c72cb2c3b500c4b894e36110b54b5f2fcfbc1
rainyjune <rainyjune@live.cn>
2016-04-16 09:24:33 +0800

    Add doublyLinkedLists.js

---
+++ Chapter6/doublyLinkedLists.js
@@ -0,0 +1,37 @@
+function Node(element) {
+  this.element = element;
+  this.previous = null;
+  this.next = null;
+}
+
+function LList() {
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+  this.display = display;
+}
+
+function find(item) {
+  var currNode = this.head;
+  while(currNode.element != item) {
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function insert(newElement, item) {
+  var newNode = new Node(newElement);
+  var current = this.find(item);
+  newNode.next = current.next;
+  newNode.previous = current;
+  current.next = newNode;
+}
+
+function display() {
+  var currNode = this.head;
+  while(currNode.next != null) {
+    console.log("Current:", currNode.next.element, " previous:", currNode.next.previous, " next:", currNode.next.next);
+    currNode = currNode.next;
+  }
+}
+
